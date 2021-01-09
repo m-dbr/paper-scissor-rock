d76a622b346edbb48f76e218823c85c002112715
mndbr <m.dbr@outlook.com>
2021-01-09 01:48:21 +0100

    main program with functions in main.py file for now.
    tests are not finished yet

---
+++ main.py
@@ -0,0 +1,96 @@
+import random
+
+
+def random_choice(options) -> str:
+    """
+    Chooses a choice randomly from the keys in options.
+    :returns: String containing the choice of the computer.
+    """
+    choices = list(options.keys())
+    return random.choice(choices)
+
+
+def check_result(player1, player2, result_dict) -> dict:
+    """
+    Check the result of the game: whether player1 wins or loose or ties with computer
+    :returns: Dictionary containing the possible results.
+    """
+    result = (player1 - player2) % 3
+
+    if result == 0:
+        result_dict['ties'] += 1
+    elif result == 1:
+        result_dict['wins'] += 1
+    elif result == 2:
+        result_dict['losses'] += 1
+    return result_dict
+
+
+def play_ngames(options, player2, n, random_pl1=True, player1=None) -> dict:
+    """
+    Play n games, check result of each game and update result_dict
+    :returns: Dictionary containing the final scores of each n games.
+    """
+    outcome_dict = {
+        "ties": 0,
+        "wins": 0,
+        "losses": 0,
+    }
+    result_dict = outcome_dict.copy()
+
+    for i in range(n):
+        if random_pl1 is True:
+            player1 = options[random_choice(options)]
+        check_result(player1, player2, result_dict)
+
+    return result_dict
+
+
+def print_score(result_dict):
+    """
+    Print the score of the play depend of data stored in result_dict dictionary.
+    """
+    print('-------- GAME OVER --------')
+    print(f"Player A (computer) wins {result_dict['wins']} times")
+    print(f"Player B (paper) wins {result_dict['losses']} times")
+    print(f"Tie: {result_dict['ties']} of 100 games")
+    print(f"Winner is: {'Player A' if result_dict['wins'] > result_dict['losses'] else 'Player B'}")
+    print('--------------------')
+
+
+def play_again():
+    """
+    Ask whether player wants to play again or exit the game.
+    """
+    while True:
+        continue_prompt = input('\nDo you wish to play again? (y/n): ').lower()
+        if continue_prompt == 'n':
+            print("Game is over")
+            exit()
+        elif continue_prompt == 'y':
+            break
+        else:
+            print("Invalid input!\n")
+            continue
+
+
+if __name__ == "__main__":
+    options = {'rock': 0, 'paper': 1, 'scissors': 2}
+    player2 = options['paper']
+    # random_pl1 when True: player1 is randomly chosen, when False player1 have to bo set
+    random_pl1 = True
+    n = 100
+
+    while True:
+        game = play_ngames(options, player2, n, random_pl1)
+        print_score(game)
+        play_again()
+
+
+
+
+
+
+
+
+
---
+++ tests.py
@@ -0,0 +1,102 @@
+import unittest
+import main
+
+
+class TestGame(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.options = {'rock': 0, 'paper': 1, 'scissors': 2}
+        cls.test_dict = {
+            "ties": 0,
+            "wins": 0,
+            "losses": 0,
+        }
+
+    def test_random_choice(self):
+        choice = main.random_choice(self.options)
+        self.assertIn(choice, list(self.options.keys()))
+
+    def test_random_wrong_choice_str(self):
+        choice = 'dog'
+        self.assertNotIn(choice, list(self.options.keys()))
+
+    def test_random_wrong_choice_int(self):
+        choice = 1
+        self.assertNotIn(choice, list(self.options.keys()))
+
+    def test_check_result_player1_wins(self):
+        player1 = self.options['scissors']
+        player2 = self.options['paper']
+        dict_test = self.test_dict.copy()
+        result = main.check_result(player1, player2, dict_test)
+
+        self.assertIn('scissors', list(self.options.keys()))
+        self.assertIn('paper', list(self.options.keys()))
+        self.assertEqual(self.options['scissors'], player1)
+        self.assertEqual(self.options['paper'], player2)
+        self.assertIn(player1, list(self.options.values()))
+        self.assertIn(player2, list(self.options.values()))
+        self.assertDictEqual(result, {"ties": 0, "wins": 1, "losses": 0}, msg='Wrong dictionary content: should be wins: 1')
+        self.assertEqual(len(result), 3)
+
+    def test_check_result_player1_losses(self):
+        player1 = self.options['rock']
+        player2 = self.options['paper']
+        dict_test = self.test_dict.copy()
+        result = main.check_result(player1, player2, dict_test)
+
+        self.assertIn('rock', list(self.options.keys()))
+        self.assertIn('paper', list(self.options.keys()))
+        self.assertEqual(self.options['rock'], player1)
+        self.assertEqual(self.options['paper'], player2)
+        self.assertIn(player1, list(self.options.values()))
+        self.assertIn(player2, list(self.options.values()))
+        self.assertDictEqual(result, {"ties": 0, "wins": 0, "losses": 1}, msg='Wrong dictionary content: should be losses: 1')
+        self.assertEqual(result['losses'], 1)
+        self.assertEqual(len(result), 3)
+
+    def test_check_result_player1_ties(self):
+        player1 = self.options['paper']
+        player2 = self.options['paper']
+        dict_test = self.test_dict.copy()
+        result = main.check_result(player1, player2, dict_test)
+
+        self.assertIn('paper', list(self.options.keys()))
+        self.assertEqual(self.options['paper'], player2)
+        self.assertIn(player1, list(self.options.values()))
+        self.assertIn(player2, list(self.options.values()))
+        self.assertDictEqual(result, {"ties": 1, "wins": 0, "losses": 0}, msg='Wrong dictionary content: should be losses: 1')
+        self.assertEqual(len(result), 3)
+
+    def test_play_ngames_player1_wins(self):
+        player1 = self.options['scissors']
+        player2 = self.options['paper']
+        n = 100
+        random_pl1 = False
+        result = main.play_ngames(self.options, player2, n, random_pl1, player1)
+
+        self.assertDictEqual(result, {"ties": 0, "wins": 100, "losses": 0})
+
+    def test_play_ngames_player1_looses(self):
+        player1 = self.options['rock']
+        player2 = self.options['paper']
+        n = 100
+        random_pl1 = False
+        result = main.play_ngames(self.options, player2, n, random_pl1, player1)
+
+        self.assertDictEqual(result, {"ties": 0, "wins": 0, "losses": 100})
+
+    def test_play_ngames_player1_ties(self):
+        player1 = self.options['paper']
+        player2 = self.options['paper']
+        n = 100
+        random_pl1 = False
+        result = main.play_ngames(self.options, player2, n, random_pl1, player1)
+
+        self.assertDictEqual(result, {"ties": 100, "wins": 0, "losses": 0})
+
+
+
+if __name__ == '__main__':
+    unittest.main()
